add5537cbdf3fa4e0c823b5a62166be4f7e043d3
Rahul <rrr392@nyu.edu>
2020-06-22 02:40:50 -0400

    added code link

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,21 +1,22 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export default class Navbar extends Component{
-    render () {
-        // navbar from boostrap documentation
-        return (
-					<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-          {/* link instead of anchor tag from react-router. 'to' param shows where it links to.*/}
-						<Link to="/" className="navbar-brand">ExcerTracker</Link>
-							<div className="navbar-collapse">
-                <ul className='navbar-nav mr-auto'>
-                  <li className='navbar-item'><Link to='/' className='nav-link'>Exercises</Link></li>
-                  <li className='navbar-item'><Link to='/create' className='nav-link'>Create Exercise Log</Link></li>
-                  <li className='navbar-item'><Link to='/user' className='nav-link'>Create User</Link></li>
-                </ul>
-              </div>
-            </nav>
-        );
-    }
+export default class Navbar extends Component {
+  render() {
+    // navbar from boostrap documentation
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        {/* link instead of anchor tag from react-router. 'to' param shows where it links to.*/}
+        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+        <div className="navbar-collapse">
+          <ul className='navbar-nav mr-auto'>
+            <li className='navbar-item'><Link to='/' className='nav-link'>Exercises</Link></li>
+            <li className='navbar-item'><Link to='/create' className='nav-link'>Create Exercise Log</Link></li>
+            <li className='navbar-item'><Link to='/user' className='nav-link'>Create User</Link></li>
+            <li className='navbar-item'><Link to="https://github.com/VoltAmpereWatt/MERN-Exercise-Tracker.git">Source Code</Link></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
